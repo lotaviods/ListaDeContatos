2a0b9789215579ec312f1d1d665c1c8fb4b8d242
lotaviods <l.otavio9099@gmail.com>
2020-12-30 10:58:33 -0300

    Adicionados os JS para requisição API

---
+++ public/js/listagem.js
@@ -0,0 +1,12 @@
+const ul = document.querySelector('#contatos');
+listarContatos().then(conteudo=>{
+    if (!conteudo.sucesso)
+        throw new Error('Falha ao carregar contatos');
+    conteudo.conteudoResposta.forEach(i =>{
+        let contato = document.createElement('li');
+        let nome = document.createTextNode(i.nome)
+        console.log(i);
+        contato.appendChild(nome);
+        ul.appendChild(contato);
+    })
+})
