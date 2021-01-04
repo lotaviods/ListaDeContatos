6e250d6c1beaa3ce66975ae10483bb2d3569ac01
CarlosFalconJ <carlos-falcon1@live.com>
2021-01-04 14:24:10 -0300

    Modificacoes referentes á implementacao do html com a adicao do cadastro.html.twig e implementacao do metodo cadastrar no controllerHtml e sua rota

--- API/src/Controller/ControllerHtml.php
+++ API/src/Controller/ControllerHtml.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Entity\Contatos;
 use App\Repository\ContatosRepository;
+use Symfony\Component\Console\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,6 +13,10 @@ class ControllerHtml extends AbstractController
     public function index()
     {
         return $this->render('Index\Index.html.twig');
+    }
+    public function cadastrar()
+    {
+        return $this->render('Index\cadastro.html.twig');
     }
 
 }
