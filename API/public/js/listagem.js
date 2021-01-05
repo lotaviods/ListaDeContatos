d93f6686ce74f76fb5f52ff23470cab8897d0ea4
CarlosFalconJ <carlos-falcon1@live.com>
2021-01-05 12:02:53 -0300

    Avançando na listagem dos contatos

--- API/public/js/listagem.js
+++ API/public/js/listagem.js
@@ -5,22 +5,34 @@ listarContatos().then(conteudo=> {
     if (!conteudo.sucesso)
         throw new Error('Falha ao carregar contatos');
     conteudo.conteudoResposta.forEach(i => {
-        let divList = document.createElement('div')
+
+        let divRow = document.createElement('div');
+        divRow.classList.add('col-sm');
+
         let ul = document.createElement('ul');
         ul.classList.add('list-group');
+
         let li = document.createElement('li');
         li.classList.add('list-group-item','col', 'col-md-2' );
-        let nome = document.createTextNode(i.nome);
 
+        let nome = document.createTextNode(i.nome);
         let email = document.createTextNode(i.email);
+        let numero = document.createTextNode(i.numero);
+
         let liEmail = document.createElement('li');
-        console.log(i);
+        liEmail.classList.add('list-group-item','col', 'col-md-2' );
+
+        let ulEmail = document.createElement('ul');
+        ulEmail.classList.add('list-group');
 
         li.appendChild(nome);
-        liEmail.appendChild(email);
-        ul.appendChild(li, liEmail);
-        divList.appendChild(li, liEmail)
+        ul.appendChild(li);
+        divRow.appendChild(ul);
 
-        div.appendChild(divList);
+        div.appendChild(divRow);
+
+        console.log(i);
+        
     })
+
 });
