042d6f04857592df3ca31ec0c572f99d6df07742
lotaviods <l.otavio9099@gmail.com>
2021-01-05 11:56:39 -0300

    Mudança de paginação

--- API/src/Helper/ExtratorDadosRequest.php
+++ API/src/Helper/ExtratorDadosRequest.php
@@ -15,7 +15,7 @@ class ExtratorDadosRequest
         $paginaAtual = array_key_exists('page', $allQuerys) ? $allQuerys['page'] : 1;
         unset($allQuerys['page']);
         
-        $itensPorPagina = array_key_exists('itensPorPagina', $allQuerys) ? $allQuerys['itensPorPagina'] : 5;
+        $itensPorPagina = array_key_exists('itensPorPagina', $allQuerys) ? $allQuerys['itensPorPagina'] : 1000;
         unset($allQuerys['itensPorPagina']);
 
         return [$infoOrdenacao, $allQuerys, $paginaAtual, $itensPorPagina];
