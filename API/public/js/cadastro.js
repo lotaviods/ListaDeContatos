65d564cc63d6b8c94b071a68171c10f79aa25f80
lotaviods <l.otavio9099@gmail.com>
2021-01-05 11:56:19 -0300

    cadastro js

--- API/public/js/cadastro.js
+++ API/public/js/cadastro.js
@@ -1,18 +1,27 @@
-export function cadastrarContato(nome, email, numero){
+export function cadastrarContato(nome, email, numero) {
     const Json = JSON.stringify({
         nome: nome,
         email: email,
         numero: numero
     })
-    return fetch('http://localhost:3000/contatos/',{
-        method: 'POST',
+    return fetch('http://localhost:8080/api/contatos/', {
+        method: "POST",
         headers: {
             'Content-type': 'application/json'
         },
         body: Json
 
-    }).then(resp=>{
-        return resp.body
+    }).then(resp => {
+        return resp.json().then(json =>{
+            return json;
+        })
     })
-
 }
+const formCadastro = document.querySelector('[data-form]');
+formCadastro.addEventListener("submit", event => {
+    event.preventDefault();
+    const nome = event.target.querySelector('[data-nome]');
+    const email = event.target.querySelector('[data-email]');
+    const num = event.target.querySelector('[data-num]');
+    cadastrarContato(nome.value, email.value, num.value);
+});
