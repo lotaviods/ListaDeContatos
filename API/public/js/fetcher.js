508235e0ff08ebd247fb1959d16e868f384801b4
lotaviods <l.otavio9099@gmail.com>
2021-01-05 08:29:46 -0300

    correção de conflitos

--- API/public/js/fetcher.js
+++ API/public/js/fetcher.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export function listarContatos(){
     try{
         return fetch('http://localhost:3000/api/contatos/',{
@@ -9,25 +9,5 @@ export function listarContatos(){
             return json;
         })
     }catch (Error){
-    console.log('Falha')
-    }
-
-
-}
-=======
-export function listarContatos(){
-    try{
-        return fetch('http://localhost:3000/api/contatos/',{
-            method: 'GET'
-        }).then(resp=> {
-            return resp.json()
-        }).then(json=>{
-            return json;
-        })
-    }catch (Error){
-    console.log('Falha')
-    }
-
-
-}
->>>>>>> d38097cbe6eec66dc1b0c4968db54a8135c2beca
+    console.log('Falha');
+    }}
--- API/public/js/listagem.js
+++ API/public/js/listagem.js
@@ -1,28 +1,13 @@
-<<<<<<< HEAD:API/public/js/listagem.js
 import {listarContatos} from './fetcher.js';
 let ul = document.querySelector('#contatos');
-listarContatos().then(conteudo=>{
+listarContatos().then(conteudo=> {
     if (!conteudo.sucesso)
         throw new Error('Falha ao carregar contatos');
-    conteudo.conteudoResposta.forEach(i =>{
+    conteudo.conteudoResposta.forEach(i => {
         let contato = document.createElement('li');
         let nome = document.createTextNode(i.nome)
         console.log(i);
         contato.appendChild(nome);
         ul.appendChild(contato);
     })
-=======
-import {listarContatos} from './fetcher.js';
-let ul = document.querySelector('#contatos');
-listarContatos().then(conteudo=>{
-    if (!conteudo.sucesso)
-        throw new Error('Falha ao carregar contatos');
-    conteudo.conteudoResposta.forEach(i =>{
-        let contato = document.createElement('li');
-        let nome = document.createTextNode(i.nome)
-        console.log(i);
-        contato.appendChild(nome);
-        ul.appendChild(contato);
-    })
->>>>>>> d38097cbe6eec66dc1b0c4968db54a8135c2beca:public/js/listagem.js
-})
+});
