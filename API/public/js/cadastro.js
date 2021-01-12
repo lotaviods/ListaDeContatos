79c10b9077d99b4d0e00af9beb49d5b12b84e1a4
Carlos <carlos.falcon@ftdata.com.br>
2021-01-12 14:00:59 -0300

    Aperfeiçoamento do listar cursos e do cadastrar

--- API/public/js/cadastro.js
+++ API/public/js/cadastro.js
@@ -1,14 +1,39 @@
 import { adiciona } from "./req/creator.js";
+
+let elements = document.getElementsByTagName("input");
+
 const formCadastro = document.querySelector('[data-form]')
     formCadastro.addEventListener("submit", event => {
-    event.preventDefault();
+        event.preventDefault();
 
-    const nome = event.target.querySelector('[data-nome]');
-    const email = event.target.querySelector('[data-email]');
-    const num = event.target.querySelector('[data-num]');
-    let resp = adiciona(nome.value, email.value, num.value);
+        const nome = event.target.querySelector('[data-nome]');
+        const email = event.target.querySelector('[data-email]');
+        const num = event.target.querySelector('[data-num]');
+        let resp = adiciona(nome.value, email.value, num.value);
 
-    resp.then(response => {
-        alert(response.conteudoResposta.mensagem);
+        resp.then(response => {
+            if(response.sucesso){
+               alert("Cadastro Realizado com sucesso");
+                limpaInput(elements);
+               return;
+            }
+            alert(response.conteudoResposta.mensagem);
+            console.log(response)
+        });
     });
-});
+function limpaInput(elements){
+    for (let i=0; i < elements.length; i++) {
+        if (elements[i].type == "text") {
+            elements[i].value = "";
+        }
+        else if (elements[i].type == "radio"){
+            elements[i].checked = false;
+        }
+        else if (elements[i].type == "checkbox"){
+            elements[i].checked = false;
+        }
+        else if (elements[i].type == "select") {
+            elements[i].selectedIndex = 0;
+        }
+    }
+}
