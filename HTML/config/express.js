0856bc5bc1b50f835ff4b3344dfb081e7306da9a
lotaviods <l.otavio9099@gmail.com>
2021-01-11 21:54:09 -0300

    corrigido html5mode

--- HTML/config/express.js
+++ HTML/config/express.js
@@ -1,9 +1,12 @@
 var express = require('express')
     ,app = express()
     ,bodyParser = require('body-parser')
-
+    ,routes = require('./routes');
+    
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+routes(app);
 
 module.exports = app;
